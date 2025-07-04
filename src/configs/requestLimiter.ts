3f9b0dafe13180c5a20d5fb996d5629b0bb30ec8
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-07-04 21:36:26 +0545

    configured rate limiter

---
+++ src/configs/requestLimiter.ts
@@ -0,0 +1,10 @@
+import rateLimit from "express-rate-limit";
+export const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+  message: {
+    error: "Too many requests. Please try again later",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import routes from "./routes/route";
 import cors from "cors";
 import CorsConfig from "./configs/corsConfig";
-const app = express();
+import { rateLimiter } from "./configs/requestLimiter";
 
+const app = express();
 const PORT = 8080;
 //middlewares
 app.use(express.json());
 app.use(cors(CorsConfig));
+app.use(rateLimiter);
 //routes
 app.use(routes);
 app.listen(PORT, () => {
